2194aa0c1b91933ba17baa9c4ca2959c4afb7ebe
Abishek H <habishek07@gmail.com>
2025-09-29 13:01:18 +0530

    add orders page filter panel and default styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import "./Home.scss";
 import WorldMap from "../../assets/images/world-map.svg";
 
+import { Link } from "react-router-dom";
+
 import { useOutletContext } from "react-router-dom";
 
 import {
@@ -262,31 +264,33 @@ function Home() {
             </div>
           </div>
 
-          <div className="summary__card orders">
-            <h3 className="overview__title">Orders</h3>
+          <Link to="/orders">
+            <div className="summary__card orders">
+              <h3 className="overview__title">Orders</h3>
 
-            <div className="summary__info">
-              <div className="summary__value">1,219</div>
-              <div className="summary__delta negative">
-                <span>-0.03%</span>
+              <div className="summary__info">
+                <div className="summary__value">1,219</div>
+                <div className="summary__delta negative">
+                  <span>-0.03%</span>
 
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M8.45488 5.60777L14 4L12.6198 9.6061L10.898 7.9532L8.12069 10.8463C8.02641 10.9445 7.89615 11 7.76 11C7.62385 11 7.49359 10.9445 7.39931 10.8463L5.36 8.72199L2.36069 11.8463C2.16946 12.0455 1.85294 12.0519 1.65373 11.8607C1.45453 11.6695 1.44807 11.3529 1.63931 11.1537L4.99931 7.65373C5.09359 7.55552 5.22385 7.5 5.36 7.5C5.49615 7.5 5.62641 7.55552 5.72069 7.65373L7.76 9.77801L10.1766 7.26067L8.45488 5.60777Z"
-                    fill="currentColor"
-                  />
-                </svg>
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M8.45488 5.60777L14 4L12.6198 9.6061L10.898 7.9532L8.12069 10.8463C8.02641 10.9445 7.89615 11 7.76 11C7.62385 11 7.49359 10.9445 7.39931 10.8463L5.36 8.72199L2.36069 11.8463C2.16946 12.0455 1.85294 12.0519 1.65373 11.8607C1.45453 11.6695 1.44807 11.3529 1.63931 11.1537L4.99931 7.65373C5.09359 7.55552 5.22385 7.5 5.36 7.5C5.49615 7.5 5.62641 7.55552 5.72069 7.65373L7.76 9.77801L10.1766 7.26067L8.45488 5.60777Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
           <div className="summary__card revenue">
             <h3 className="overview__title">Revenue</h3>
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,7 +1,63 @@
 import "./Orders.scss";
 
+import { useRef } from "react";
+
+import { AddIcon } from "../../icons/AddIcon";
+import { FunnelIcon } from "../../icons/FunnelIcon";
+import { SortIcon } from "../../icons/SortIcon";
+import { SearchIcon } from "../../icons/SearchIcon";
+
 function Orders() {
-  return <div className="Orders">Orders</div>;
+  const inputRef = useRef(null);
+
+  function handleFocus() {
+    inputRef.current?.focus();
+  }
+
+  return (
+    <div className="orders">
+      <h2 className="orders__caption">Order List</h2>
+
+      <div className="orders__table">
+        <div className="orders__refine-panel">
+          <div className="orders__filter">
+            <button className="orders__filter-btn">
+              <AddIcon width={20} height={20} />
+            </button>
+
+            <button className="orders__filter-btn">
+              <FunnelIcon width={20} height={20} />
+            </button>
+
+            <button className="orders__filter-btn">
+              <SortIcon width={20} height={20} />
+            </button>
+          </div>
+
+          <div className="orders__search">
+            <div className="orders__search-bar">
+              <button
+                type="button"
+                aria-label="Focus Search"
+                onClick={handleFocus}
+                className="orders__search-icon"
+              >
+                <SearchIcon width={16} height={16} />
+              </button>
+
+              <input
+                type="text"
+                className="orders__search-input"
+                id="orders-search"
+                placeholder="Search"
+                ref={inputRef}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
